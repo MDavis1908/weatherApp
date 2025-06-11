ce0c7bdc5d5907335d0e31b4c181517f092f5301
Matthew <matthewdavis92@gmail.com>
2025-06-11 19:35:31 +0100

    Created script.JS and started building interaction skeleton

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("user-input");
+const submit = document.getElementById("user-submit");
+const results = document.getElementById("results-container");
+
+submit.addEventListener('click', function(event){
+    let cityName = input.textContent;
+})
