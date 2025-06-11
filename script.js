ed418f8c23ca79314029d681d9a9e3bf32cd6e50
Matthew <matthewdavis92@gmail.com>
2025-06-11 20:09:15 +0100

    Added function to fetch weather data using openweathermap API

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const input = document.getElementById("user-input");
 const submit = document.getElementById("user-submit");
 const results = document.getElementById("results-container");
-
+const APIKey = "384e15f6cfe26fa731cc10bbc1a2115c";
+// Listens for the submit button to be clicked and assigns the value in textbox to cityName variable
 submit.addEventListener('click', function(event){
-    let cityName = input.textContent;
+    let cityName = input.value;
+    console.log(cityName);
+    getWeather(cityName);
 })
+
+async function getWeather(cityName) {
+    try {
+        apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey + "&units=metric"
+        console.log(apiURL);
+        const response = await fetch(apiURL);
+        if (response.ok != true) {
+            throw new Error("Could not fetch resource");
+        }
+        const data = await response.json();
+        console.log(data);
+        displayWeather(data);
+    } catch (error) {
+
+    }
+}
+
+function displayWeather() {
+    const name = data.name;
+    const temp = data.main.temp;
+    const description = data.weather[0].description;
+}
